b60d9e86d1d96c9023395491d861d7a494dfc6a7
0xfouda <0xfouda@gmail.com>
2018-06-20 13:18:48 +0300

    Initial Upload

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WindowsFormsApplication1
 {
-    partial class Form1
+    partial class MemRW
     {
         /// <summary>
         /// Required designer variable.
@@ -28,45 +28,278 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_memadd = new System.Windows.Forms.Label();
+            this.txtbx_memadd = new System.Windows.Forms.TextBox();
+            this.txtbx_len = new System.Windows.Forms.TextBox();
+            this.lbx_allprocs = new System.Windows.Forms.ListBox();
+            this.txtbx_hx = new System.Windows.Forms.TextBox();
+            this.txtbx_ascii = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btn_inject = new System.Windows.Forms.Button();
+            this.txtbx_dllpath = new System.Windows.Forms.TextBox();
+            this.btn_browse = new System.Windows.Forms.Button();
+            this.fbd = new System.Windows.Forms.OpenFileDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btn_injectshellcode = new System.Windows.Forms.Button();
+            this.txtbx_shellcode = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(190, 41);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(200, 20);
-            this.textBox1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(39, 39);
+            this.button1.Location = new System.Drawing.Point(151, 44);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(130, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Create License";
+            this.button1.Size = new System.Drawing.Size(195, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Read Memory";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // Form1
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(151, 73);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(267, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Write Memory";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(151, 102);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(267, 94);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Process List";
+            // 
+            // lbl_memadd
+            // 
+            this.lbl_memadd.AutoSize = true;
+            this.lbl_memadd.Location = new System.Drawing.Point(155, 16);
+            this.lbl_memadd.Name = "lbl_memadd";
+            this.lbl_memadd.Size = new System.Drawing.Size(85, 13);
+            this.lbl_memadd.TabIndex = 4;
+            this.lbl_memadd.Text = "Memory Address";
+            // 
+            // txtbx_memadd
+            // 
+            this.txtbx_memadd.BackColor = System.Drawing.Color.Black;
+            this.txtbx_memadd.ForeColor = System.Drawing.Color.Lime;
+            this.txtbx_memadd.Location = new System.Drawing.Point(246, 13);
+            this.txtbx_memadd.Name = "txtbx_memadd";
+            this.txtbx_memadd.Size = new System.Drawing.Size(100, 20);
+            this.txtbx_memadd.TabIndex = 1;
+            // 
+            // txtbx_len
+            // 
+            this.txtbx_len.Location = new System.Drawing.Point(352, 46);
+            this.txtbx_len.Name = "txtbx_len";
+            this.txtbx_len.Size = new System.Drawing.Size(55, 20);
+            this.txtbx_len.TabIndex = 3;
+            // 
+            // lbx_allprocs
+            // 
+            this.lbx_allprocs.FormattingEnabled = true;
+            this.lbx_allprocs.Location = new System.Drawing.Point(15, 25);
+            this.lbx_allprocs.Name = "lbx_allprocs";
+            this.lbx_allprocs.Size = new System.Drawing.Size(120, 225);
+            this.lbx_allprocs.Sorted = true;
+            this.lbx_allprocs.TabIndex = 5;
+            // 
+            // txtbx_hx
+            // 
+            this.txtbx_hx.BackColor = System.Drawing.Color.Black;
+            this.txtbx_hx.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbx_hx.ForeColor = System.Drawing.Color.Lime;
+            this.txtbx_hx.Location = new System.Drawing.Point(424, 25);
+            this.txtbx_hx.Multiline = true;
+            this.txtbx_hx.Name = "txtbx_hx";
+            this.txtbx_hx.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtbx_hx.Size = new System.Drawing.Size(230, 173);
+            this.txtbx_hx.TabIndex = 6;
+            // 
+            // txtbx_ascii
+            // 
+            this.txtbx_ascii.BackColor = System.Drawing.Color.Black;
+            this.txtbx_ascii.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbx_ascii.ForeColor = System.Drawing.Color.Lime;
+            this.txtbx_ascii.Location = new System.Drawing.Point(660, 25);
+            this.txtbx_ascii.Multiline = true;
+            this.txtbx_ascii.Name = "txtbx_ascii";
+            this.txtbx_ascii.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtbx_ascii.Size = new System.Drawing.Size(125, 174);
+            this.txtbx_ascii.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(421, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Hex View";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(657, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "ASCII View";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(360, 30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Length";
+            // 
+            // btn_inject
+            // 
+            this.btn_inject.Location = new System.Drawing.Point(149, 202);
+            this.btn_inject.Name = "btn_inject";
+            this.btn_inject.Size = new System.Drawing.Size(267, 23);
+            this.btn_inject.TabIndex = 4;
+            this.btn_inject.Text = "Inject DLL";
+            this.btn_inject.UseVisualStyleBackColor = true;
+            this.btn_inject.Click += new System.EventHandler(this.btn_inject_Click);
+            // 
+            // txtbx_dllpath
+            // 
+            this.txtbx_dllpath.Location = new System.Drawing.Point(422, 205);
+            this.txtbx_dllpath.Name = "txtbx_dllpath";
+            this.txtbx_dllpath.Size = new System.Drawing.Size(283, 20);
+            this.txtbx_dllpath.TabIndex = 3;
+            // 
+            // btn_browse
+            // 
+            this.btn_browse.Location = new System.Drawing.Point(711, 205);
+            this.btn_browse.Name = "btn_browse";
+            this.btn_browse.Size = new System.Drawing.Size(74, 23);
+            this.btn_browse.TabIndex = 4;
+            this.btn_browse.Text = "Browse";
+            this.btn_browse.UseVisualStyleBackColor = true;
+            this.btn_browse.Click += new System.EventHandler(this.btn_browse_Click);
+            // 
+            // fbd
+            // 
+            this.fbd.FileName = "injected.dll";
+            this.fbd.Filter = "DLL files|*.dll";
+            this.fbd.ReadOnlyChecked = true;
+            this.fbd.Title = "Choose DLL To Inject";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(15, 260);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(120, 22);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Refresh";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Form1_Load);
+            // 
+            // btn_injectshellcode
+            // 
+            this.btn_injectshellcode.Location = new System.Drawing.Point(149, 231);
+            this.btn_injectshellcode.Name = "btn_injectshellcode";
+            this.btn_injectshellcode.Size = new System.Drawing.Size(267, 23);
+            this.btn_injectshellcode.TabIndex = 4;
+            this.btn_injectshellcode.Text = "Inject Shellcode \"CreateRemoteThread Method\"";
+            this.btn_injectshellcode.UseVisualStyleBackColor = true;
+            this.btn_injectshellcode.Click += new System.EventHandler(this.btn_injectshellcode_Click);
+            // 
+            // txtbx_shellcode
+            // 
+            this.txtbx_shellcode.Location = new System.Drawing.Point(422, 234);
+            this.txtbx_shellcode.Multiline = true;
+            this.txtbx_shellcode.Name = "txtbx_shellcode";
+            this.txtbx_shellcode.Size = new System.Drawing.Size(363, 48);
+            this.txtbx_shellcode.TabIndex = 3;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(149, 259);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(267, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Inject Shellcode \"Method#2\"";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.btn_injectshellcode_Click);
+            // 
+            // MemRW
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(458, 129);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(797, 294);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.txtbx_ascii);
+            this.Controls.Add(this.txtbx_hx);
+            this.Controls.Add(this.lbx_allprocs);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbl_memadd);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtbx_memadd);
+            this.Controls.Add(this.txtbx_dllpath);
+            this.Controls.Add(this.txtbx_len);
+            this.Controls.Add(this.txtbx_shellcode);
             this.Controls.Add(this.textBox1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Controls.Add(this.btn_browse);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.btn_injectshellcode);
+            this.Controls.Add(this.btn_inject);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "MemRW";
+            this.Text = "Memory RW";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_memadd;
+        private System.Windows.Forms.TextBox txtbx_memadd;
+        private System.Windows.Forms.TextBox txtbx_len;
+        private System.Windows.Forms.ListBox lbx_allprocs;
+        private System.Windows.Forms.TextBox txtbx_hx;
+        private System.Windows.Forms.TextBox txtbx_ascii;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btn_inject;
+        private System.Windows.Forms.TextBox txtbx_dllpath;
+        private System.Windows.Forms.Button btn_browse;
+        private System.Windows.Forms.OpenFileDialog fbd;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btn_injectshellcode;
+        private System.Windows.Forms.TextBox txtbx_shellcode;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,20 +7,178 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using htt
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
 
 namespace WindowsFormsApplication1
 {
-    public partial class Form1 : Form
+
+    public partial class MemRW : Form
     {
-        public Form1()
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool ReadProcessMemory(int hProcess,
+          int lpBaseAddress, byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesRead);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool WriteProcessMemory(int hProcess, int lpBaseAddress,
+            char[] lpBuffer, int dwSize, ref int lpNumberOfBytesWritten);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        [DllImport("kernel32", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)]
+        static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        static extern IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress,
+            uint dwSize, uint flAllocationType, uint flProtect);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, uint nSize, out UIntPtr lpNumberOfBytesWritten);
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr CreateRemoteThread(IntPtr hProcess,
+            IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
+
+        // privileges
+        const int PROCESS_CREATE_THREAD = 0x0002;
+        const int PROCESS_QUERY_INFORMATION = 0x0400;
+        const int PROCESS_VM_OPERATION = 0x0008;
+        const int PROCESS_VM_WRITE = 0x0020;
+        const int PROCESS_VM_READ = 0x0010;
+
+        // used for memory allocation
+        const uint MEM_COMMIT = 0x00001000;
+        const uint MEM_RESERVE = 0x00002000;
+        const uint PAGE_READWRITE = 4;
+        const uint PAGE_READWRITEEXEC = 0x40;
+        public MemRW()
         {
             InitializeComponent();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+            if (lbx_allprocs.SelectedItem != null)
+            {
+                Process process = Process.GetProcessesByName(lbx_allprocs.SelectedItem.ToString())[0];
+                IntPtr processHandle = OpenProcess(PROCESS_VM_READ, false, process.Id);
+
+                int bytesRead = (txtbx_len.Text == "") ? 5 : Convert.ToInt32(txtbx_len.Text);
+                byte[] buffer = new byte[bytesRead * 2]; //'Hello World!' takes 12*2 bytes because of Unicode 
+                int memadd = int.Parse(txtbx_memadd.Text, System.Globalization.NumberStyles.HexNumber);
+                ReadProcessMemory((int)processHandle, memadd, buffer, buffer.Length, ref bytesRead);
+                string hex = BitConverter.ToString(buffer);
+                txtbx_hx.Text = hex.Replace('-', '\t');
+                txtbx_ascii.Text = Encoding.ASCII.GetString(buffer);
+                //MessageBox.Show(chars.ToString());
+            }
+            else
+            {
+                MessageBox.Show("Please Select a Process");
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            if (lbx_allprocs.SelectedItem != null)
+            {
+            Process process = Process.GetProcessesByName(lbx_allprocs.SelectedItem.ToString())[0];
+            IntPtr processHandle = OpenProcess(PROCESS_VM_READ | PROCESS_VM_OPERATION | PROCESS_VM_WRITE, false, process.Id);
+            int bytesWrite = textBox1.Text.Length;
+            char[] txt = textBox1.Text.ToString().ToCharArray();
+            int memadd = int.Parse(txtbx_memadd.Text, System.Globalization.NumberStyles.HexNumber);
+
+            WriteProcessMemory((int)processHandle, memadd, txt, txt.Length, ref bytesWrite);
+
+            MessageBox.Show(textBox1.Text + " has been written to memory!");
+            }
+            else
+            {
+                MessageBox.Show("Please Select a Process");
+            }
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (Process proc in Process.GetProcesses())
+            {
+                lbx_allprocs.Items.Add(proc.ProcessName);
+            }
+        }
+
+        private void btn_browse_Click(object sender, EventArgs e)
+        {
+            fbd.ShowDialog();
+            txtbx_dllpath.Text = fbd.FileName;
+        }
+
+        private void btn_inject_Click(object sender, EventArgs e)
+        {
+            if (lbx_allprocs.SelectedItem != null)
+            {
+                if (txtbx_dllpath.Text.ToUpper().EndsWith("DLL"))
+                {
+                    Process process = Process.GetProcessesByName(lbx_allprocs.SelectedItem.ToString())[0];
+                    IntPtr processHandle = OpenProcess(PROCESS_VM_READ | PROCESS_VM_WRITE | PROCESS_VM_OPERATION | PROCESS_QUERY_INFORMATION | PROCESS_CREATE_THREAD, false, process.Id);
+                    IntPtr loadLibraryAddr = GetProcAddress(GetModuleHandle("kernel32.dll"), "LoadLibraryA");
+                    IntPtr allocAddr = VirtualAllocEx(processHandle, IntPtr.Zero, (uint)((txtbx_dllpath.Text.Length + 1) * Marshal.SizeOf(typeof(char))), MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+                    int bytesWritten = txtbx_dllpath.Text.Length;
+                    WriteProcessMemory((int)processHandle, (int)allocAddr, txtbx_dllpath.Text.ToCharArray(), txtbx_dllpath.Text.Length, ref bytesWritten);
+                    CreateRemoteThread(processHandle, IntPtr.Zero, 0, loadLibraryAddr, allocAddr, 0, IntPtr.Zero);
+
+                }
+                else
+                {
+                    MessageBox.Show("Please Select a valid DLL!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please Select a Process to Inject!");
+            }
+        }
+
+        private void btn_injectshellcode_Click(object sender, EventArgs e)
+        {
+            if (lbx_allprocs.SelectedItem != null)
+            {
+                
+                    if (txtbx_shellcode.Text.Length > 0)
+                    {
+                        char[] shellcode = Encoding.Default.GetChars(Convert.FromBase64String(txtbx_shellcode.Text));
+                        MessageBox.Show(BitConverter.ToString(Encoding.Default.GetBytes(shellcode)));
+                        //get process handle
+                        Process process = Process.GetProcessesByName(lbx_allprocs.SelectedItem.ToString())[0];
+                        IntPtr processHandle = OpenProcess(PROCESS_VM_READ | PROCESS_VM_WRITE | PROCESS_QUERY_INFORMATION | PROCESS_VM_OPERATION | PROCESS_CREATE_THREAD, false, process.Id);
+                        //Allocate Memory with RWX
+                        IntPtr AllocatedSpaceAddr = VirtualAllocEx(processHandle, IntPtr.Zero, (uint)shellcode.Length, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITEEXEC);
+                        //Write Shellcode into Allocated Space
+                        int bytesWritten = shellcode.Length;
+                        WriteProcessMemory((int)processHandle,(int) AllocatedSpaceAddr, shellcode, shellcode.Length, ref bytesWritten);
+                        //Execute Shellcode
+                        IntPtr threadID = CreateRemoteThread(processHandle, IntPtr.Zero, 0, AllocatedSpaceAddr,IntPtr.Zero,0,IntPtr.Zero);
+                        MessageBox.Show("Process " + process.ProcessName + " Injected, Thread " + threadID.ToString() + " Created !!");
+                    }
+                
+                else
+                {
+                    MessageBox.Show("Please Select a valid DLL!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please Select a Process to Inject!");
+            }
         }
     }
 }
